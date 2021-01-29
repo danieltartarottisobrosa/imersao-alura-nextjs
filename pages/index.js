d4355a65e9bdd8f0279372120235757fe87ecf15
Daniel T. Sobrosa <danieltsobrosa@gmail.com>
2021-01-28 22:41:08 -0300

    Corrige link do repo

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl={db.repo} />
     </QuizBackground>
   );
 }
